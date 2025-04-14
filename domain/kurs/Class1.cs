2ae113bf3551e9d4d368fa9a6a27251bf8902a21
palka <esongolova@gmail.com>
2025-04-14 22:56:28 +0700

    start

---
+++ domain/kurs/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace kurs;
+
+public class Book
+{
+    public int Id { get; }
+    public string Title { get; }
+    public Book(int id, string title)
+    {
+        Id = id;
+        Title = title;
+    }
+}
