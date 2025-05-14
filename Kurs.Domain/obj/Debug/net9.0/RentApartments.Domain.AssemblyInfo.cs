4a4bf3c0ec70bafa642019f03c416fe2e0e2a163
palka <esongolova@gmail.com>
2025-05-14 23:37:28 +0700

    no comments(literally)

--- Kurs.Domain/obj/Debug/net9.0/RentApartments.Domain.AssemblyInfo.cs
+++ Kurs.Domain/obj/Debug/net9.0/RentApartments.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentApartments.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7aa36637fe611b33375a70f1b1fb074ca1e60cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e3188877798555906010697cb6c768539973a67")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentApartments.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentApartments.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- RentApartments.Domain.Repositories.Abstractions/ILandlordRepository.cs
+++ RentApartments.Domain.Repositories.Abstractions/ILandlordRepository.cs
@@ -6,12 +6,13 @@ namespace RentApartments.Domain.Repositories.Abstractions
 {
     public interface ILandlordRepository : IRepository<Landlord, Guid>
     {
+        Task<Landlord?> GetLandlordByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task<Landlord?> AddLandlordAsync(Landlord landlord, CancellationToken cancellationToken);
+        Task<bool> DeleteLandlordAsync(Landlord landlord, CancellationToken cancellationToken);
+
         Task<Landlord?> GetByUsernameAsync(string username, CancellationToken cancellationToken);
-        Task<Landlord?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
         Task<Landlord?> GetByUsernameAsync(Username username, CancellationToken cancellationToken);
         Task<IEnumerable<Landlord>> GetAllAsync(CancellationToken cancellationToken);
-        Task<Landlord?> AddAsync(Landlord landlord, CancellationToken cancellationToken);
-        Task<bool> DeleteAsync(Landlord landlord, CancellationToken cancellationToken);
-
     }
+
 }
